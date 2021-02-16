69ad7bea1614448cf9e24798a7d3095a0fd37d3c
Sjoerd Vink <sjoerdvink@icloud.com>
2021-02-16 13:47:47 +0100

    First commit

---
+++ src/spotify.js
@@ -0,0 +1,27 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+const clientId = "2a99b3652bb243c6b192b7fc9370c4af";
+const redirectUri = "https://spotify-clone-a0881.web.app";
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+];
+
+export const getTokenFromResponse = () => {
+  return window.location.hash
+    .substring(1)
+    .split("&")
+    .reduce((initial, item) => {
+      var parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+
+      return initial;
+    }, {});
+};
+
+export const accessUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
